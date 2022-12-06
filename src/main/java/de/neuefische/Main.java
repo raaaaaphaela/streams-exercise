05c106ba7556b8ece339e2fd0c5f79a023565b1d
Raphaela <raphaela.dohrmann@gmail.com>
2022-12-06 14:29:57 +0100

    Refactoring

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -3,6 +3,7 @@ package de.neuefische;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -27,21 +28,35 @@ public class Main {
         System.out.println(productOfNumbers);
 
         // Bonus
+        parseCSV().forEach(System.out::println);
+    }
+
+    public static List<Student> parseCSV() {
+        List<Student> students = new ArrayList<>();
         try {
             Path path = Path.of("students.csv");
 
-            List<Student> lines = Files.lines(path)
+            students = Files.lines(path)
                     .skip(1)
                     .filter(x -> !x.equals(""))
                     .distinct()
                     .map(line -> line.split(","))
                     .filter(x -> x.length == 4)
+                    .filter(x -> {
+                        try {
+                            Integer.parseInt(x[0]);
+                            Integer.parseInt(x[3]);
+                            return true;
+                        } catch (NumberFormatException e) {
+                            return false;
+                        }
+                    })
                     .map(data -> new Student(Integer.parseInt(data[0]), data[1], Integer.parseInt(data[2]), Integer.parseInt(data[3]))).toList();
-
-            lines.forEach(System.out::println);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return students;
     }
 }
